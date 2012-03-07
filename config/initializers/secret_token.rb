0ebe229d38c3c38466bb588d0726b81aefd5978d
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2012-03-07 10:22:58 +0100

    get secret token for signed coockies in ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-StopWatch::Application.config.secret_token = '1748e28c51e872f9c683fd4574d8c458883735f919a50e09fa593e6f4a146eca4f8840e93a949d2943ebc7bd785479b1a2c97e600d7aa6677a1358c2556577f4'
+StopWatch::Application.config.secret_token = ENV['SIGNED_COOKIE_SECRET_TOKEN']
